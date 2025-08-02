6c983921d70132216c0c18592b9a4b9f9ef938b5
Jacobtread <jacobtread@gmail.com>
2025-08-02 22:25:27 +1200

    feat: counter example project

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,54 @@
-import tilepad from "tilepad-sdk";
+import tilepad, { Inspector } from "@tilepad/sdk";
 
-tilepad.on("properties", ({ properties }) => {
-  console.debug("Got properties", properties);
+interface Properties {
+  counter?: number;
+}
+
+let currentInspector: Inspector | null = null;
+
+let counter = 0;
+
+function increase(amount: number) {
+  counter += amount;
+  if (currentInspector) {
+    currentInspector.send({ type: "COUNTER", value: counter });
+  }
+}
+
+function decrease(amount: number) {
+  counter -= amount;
+  if (currentInspector) {
+    currentInspector.send({ type: "COUNTER", value: counter });
+  }
+}
+
+tilepad.getProperties<Properties>().then((properties) => {
+  counter = Number(properties.counter ?? 0);
+});
+
+tilepad.on("inspector_open", ({ inspector }) => {
+  currentInspector = inspector;
 });
 
 tilepad.on("inspector_message", ({ inspector, message }) => {
   console.debug("Got inspector message", inspector.ctx, message);
 
   switch (message.type) {
-    case "PING": {
-      inspector.send({ type: "PONG", message: "Got your ping" });
+    case "GET_COUNTER": {
+      inspector.send({ type: "COUNTER", value: counter });
+      break;
+    }
+  }
+});
+
+tilepad.on("tile_clicked", ({ ctx, properties }) => {
+  switch (ctx.action_id) {
+    case "increase": {
+      increase(Number(properties.amount ?? 1));
+      break;
+    }
+    case "decrease": {
+      decrease(Number(properties.amount ?? 1));
       break;
     }
   }
